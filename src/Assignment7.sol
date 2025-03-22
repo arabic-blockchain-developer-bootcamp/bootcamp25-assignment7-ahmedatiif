41f046b720c6427c9f648b9c9b566575d3c22832
ahmedatiif <aatef0699@gmail.com>
2025-03-22 07:03:00 +0200

    Update Assignment7.sol

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -4,12 +4,18 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Assignment7 is ERC20, Ownable {
-    // Constructor to initialize the token with a name and symbol
-    constructor(string memory _name , string memory _symbol) ERC20(_name, _symbol) Ownable (msg.sender){}
+contract Assignment7 is ERC20, Ownable{
+    // Constructor to initialize the token
+    // Fill in the parameters for the token name and symbol
+     constructor(string memory _name , string memory _symbol) ERC20(_name, _symbol) Ownable (msg.sender){
+     }
 
     // Function to mint tokens
-    function mint(address to, uint256 amount) public  onlyOwner {
+    // Fill in the visibility modifier to be external
+    // Set params as to address and amount
+     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+        // Fill in the logic
+        // call _mint function with to and amount params
     }
 }
