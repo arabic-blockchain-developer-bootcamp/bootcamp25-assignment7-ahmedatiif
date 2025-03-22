431a7b580ef5c0cbe64230ea3392037198e6184c
ahmedatiif <aatef0699@gmail.com>
2025-03-22 06:54:17 +0200

    Update Assignment7.sol

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.26;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Assignment7 is ERC20, Ownable {
     // Constructor to initialize the token with a name and symbol
-    constructor(string memory _name , string memory _symbol) ERC20("_name", "_symbol") Ownable (msg.sender){}
+    constructor(string memory _name , string memory _symbol) ERC20(_name, _symbol) Ownable (msg.sender){}
 
     // Function to mint tokens
     function mint(address to, uint256 amount) public  onlyOwner {
